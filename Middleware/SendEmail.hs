8c35297a76861ecfb713ea240565ea85d6fbab20
刘闽晟 <notcome@me.com>
2015-01-26 12:13:21 +0800

    switch to test SendEmail

--- Middleware/SendEmail.hs
+++ Middleware/SendEmail.hs
@@ -5,6 +5,7 @@ import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import Network.Mail.SMTP
 
+{-
 sendNotification :: String -> String -> String -> IO ()
 sendNotification recipent title content = do
   let recipent' = T.pack recipent
@@ -13,3 +14,11 @@ sendNotification recipent title content = do
   sendMailWithLogin' "localhost" 587 "testuser" "testpassword" $ simpleMail
     (Address (Just "Yan.ac 消息通知") "notifications@yan.ac")
     [Address Nothing recipent'] [] [] title' [plainTextPart content']
+-}
+
+sendNotification :: String -> String -> String -> IO ()
+sendNotification recipent title content = do
+  putStrLn $ "Recipent: " ++ recipent
+  putStrLn $ "Title:    " ++ title
+  putStrLn $ "Content:"
+  putStrLn content
