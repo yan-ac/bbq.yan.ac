f06973c775392285390b66770f6dc23b814759e4
刘闽晟 <notcome@me.com>
2015-01-29 10:03:14 +0800

    move to ValidatableForm

--- Page/Registration.hs
+++ Page/Registration.hs
@@ -19,6 +19,7 @@ import Data.Maybe
 import Data.Char
 
 import Data.RequestState
+import Data.ValidatableForm
 import Data.BBQ
 import Data.VCodePool
 import Acid.BBQ
@@ -44,6 +45,7 @@ simpleResponse template msg = toResponse $ template msg ( H.h1 $ do H.toHtml msg
 vrEmail = isValid . pack
 vrPassword = inRange 12 24 . length
 
+thenThrowError cond err = if cond then left err else right ()
 elseThrowError cond err = if cond then right () else left err
 extractEither e = case e of
   Left a  -> left a
@@ -63,25 +65,30 @@ mkRegisterPage = do
     Nothing -> ok $ toResponse $ template "注册" registerPage
 
 -- Send Verification Link --
-mkFillInfoLink email vcode = "/fill-info?email=" ++ email ++ "&vcode=" ++ vcode
+mkFillInfoLink (Email email) (VCode vcode) = "/fill-info?email=" ++ email ++ "&vcode=" ++ vcode
 
 sendVerificationLink = do
-  template  <- askBasicTemplate
-  email'    <- fetchEmail
-  case email' of
-    Left errMsg -> badRequest $ simpleResponse template errMsg
-    Right email -> do
-      vcode      <- liftIO $ getNextVCode
-      expireTime <- liftIO $ expireIn (Second 3600)
-      update $ InsertNewAccount (Email email) vcode expireTime
-      let url = "https://bbq.yan.ac" ++ (mkFillInfoLink email (unVCode vcode))
-      liftIO $ sendNotification email "BBQ.Yan.ac 账户注册"
-        (  "有人使用该邮箱地址在 BBQ.Yan.ac 上注册账户。"
-        ++ "如果这不是你本人的行为，请忽略此邮件。"
-        ++ "否则，请点击链接完成你的注册：" ++ url
-        )
-      ok $ simpleResponse template "请登录邮箱以完成注册"
+  template <- askBasicTemplate
+  postData <- getValidatorFromValidatableForm mkSafeRegisterForm
 
+  result <- runEitherT $ do
+    email        <- extractEither postData
+    isRegistered <- lift $ query $ IsEmailRegisterd email
+    thenThrowError isRegistered "该邮箱已被注册"
+
+    vcode      <- liftIO $ getNextVCode
+    expireTime <- liftIO $ expireIn (Second 3600)
+    lift $ update $ InsertNewAccount email vcode expireTime
+    let url = "https://bbq.yan.ac" ++ (mkFillInfoLink email vcode)
+    liftIO $ sendNotification email "BBQ.Yan.ac 账户注册"
+      (  "有人使用该邮箱地址在 BBQ.Yan.ac 上注册账户。"
+      ++ "如果这不是你本人的行为，请忽略此邮件。"
+      ++ "否则，请点击链接完成你的注册：" ++ url
+      )
+
+  case result of
+    Left errMsg -> badRequest $ simpleResponse template errMsg
+    Right _     -> ok $ simpleResponse template "请登录邮箱以完成注册"
 
 -- Make Fill Info Page --
 mkFillInfoPage = do
@@ -93,18 +100,16 @@ mkFillInfoPage = do
 
 -- Make New Account --
 mkNewAccount = do
-  template      <- askBasicTemplate
-  authResult    <- fetchEmailVCode
-  personalInfo' <- fetchPersonalInfoForm
-  password'     <- fetchPassword
+  template   <- askBasicTemplate
+  authResult <- fetchEmailVCode
+  postData   <- getValidatorFromValidatableForm mkSafeFillInfoForm
 
   result <- runEitherT $ do
-    (email, _)   <- extractEither authResult
-    personalInfo <- extractEither personalInfo'
-    plainPwd     <- extractEither password'
-    cryptedPwd   <- liftIO $ hashPasswordUsingPolicy fastBcryptHashingPolicy (pack plainPwd)
-    let finalPwd = unpack . fromJust $ cryptedPwd
-    lift $ update $ NewAccount (Email email, Password finalPwd) personalInfo
+    (email, _)  <- extractEither authResult
+    (pwd, info) <- extractEither postData
+    cryptedPwd  <- liftIO $ hashPasswordUsingPolicy fastBcryptHashingPolicy (pack . unPassword $ pwd)
+    let finalPwd = Password $ unpack . fromJust $ cryptedPwd
+    lift $ update $ NewAccount (email, finalPwd) info
 
   case result of
     Left errMsg -> badRequest $ simpleResponse template errMsg
@@ -112,65 +117,41 @@ mkNewAccount = do
 
 -- Verification --
 fetchEmailVCode = do
-  email      <- queryString $ look "email"
-  vcode      <- queryString $ look "vcode"
+  email'     <- queryString $ look "email"
+  vcode'     <- queryString $ look "vcode"
+  let email  = Email email'
+  let vcode  = VCode vcode'
   now        <- liftIO $ getCurrentTimeInSecond
-  authResult <- query $ VerifyNewAccount (Email email) (VCode vcode) (ExpireTime now)
+  authResult <- query $ VerifyNewAccount email vcode (ExpireTime now)
   case authResult of
     Left errMsg -> return (Left errMsg)
     Right _     -> return (Right (email, vcode))
 
-fetchEmail = do
-  email      <- body $ look "email"
-  runEitherT $ do
-    elseThrowError (vrEmail email) "错误的邮箱地址"
-    isRegistered <- lift $ query $ IsEmailRegisterd (Email email)
-    elseThrowError (not isRegistered) "该邮箱已被注册"
-    right email
-
-fetchPassword  = do
-  password <- body $ look "password"
-  if vrPassword password
-    then return (Right password)
-    else return (Left "密码应当为 12—24 位")
-
-fetchPersonalInfoForm = do
-  realname <- body $ look "realname"
-  school   <- body $ look "school"
-  grade    <- body $ look "grade"
-  let test lo hi x = (all isHanChar x) && (inRange lo hi (length x))
-  runEitherT $ do
-    elseThrowError (test 2 6 realname) "姓名格式不正确"
-    elseThrowError (test 4 20 school)  "校名格式不正确"
-    elseThrowError (grade `elem` ["初一", "初二", "初三", "高一", "高二", "高三"]) "年级格式不正确"
-    right (realname, school, grade)
-
 -- Pages --
+mkSafeRegisterForm :: (Monad m, TypesafeForm m) => m Email
+mkSafeRegisterForm = do
+  email     <- askEmail "email" "填写你的邮件地址"
+  addButton "register" "发送验证邮件"
+  return email
+
 registerPage = do
-  H.h1 "填写你的邮件地址"
-  H.form ! A.action "/register"
-         ! A.method "post" $ do
-    H.label ! A.for "email" $ do "邮件地址"
-    H.input ! A.type_ "email"
-            ! A.name  "email"
-    H.button ! A.type_ "submit"
-             ! A.name "register" $ do "发送验证邮件"
+  H.h1 "注册友谊赛"
+  getHtmlFromValidatableForm "/register" mkSafeRegisterForm
+
+mkSafeFillInfoForm :: (Monad m, TypesafeForm m) => m (Password, (String, String, String))
+mkSafeFillInfoForm = do
+  password  <- askPassword "password" "输入你的密码（12—24 位，任意字符均可）"
+  repeat    <- askPassword "repeat"   "重复输入你的密码"
+  realName  <- askText     "realname" "输入你的真实姓名，全汉字，不超过六个汉字"
+  school    <- askText     "school"   "输入你的学校的中文全称，全汉字，不超过二十个汉字"
+  grade     <- askChoice   "grade"    "选择你的年级" [(x, x) | x <- ["初一", "初二", "初三", "高一", "高二", "高三"]]
+  addButton "submit" "完成注册"
+  should (password == repeat) (Just "两次密码输入不一致")
+  let test lo hi x = all isHanChar x && inRange lo hi (length x)
+  should (test 2 6  realName) (Just "姓名格式不正确")
+  should (test 4 20 school)   (Just "校名格式不正确")
+  return (password, (realName, school, grade))
 
 fillInfoPage email vcode = do
   H.h1 "填写密码及个人信息"
-  H.form ! A.action (stringValue $ mkFillInfoLink email vcode)
-         ! A.method "post" $ do
-    H.label ! A.for "password" $ do "密码（12—24 位）"
-    H.input ! A.type_ "password"
-            ! A.name  "password"
-    H.label ! A.for "realname" $ do "真实姓名"
-    H.input ! A.type_ "text"
-            ! A.name "realname"
-    H.label ! A.for "school" $ do "学校"
-    H.input ! A.type_ "text"
-            ! A.name "school"
-    H.label ! A.for "grade" $ do "年级"
-    H.input ! A.type_ "text"
-            ! A.name "grade"
-    H.button ! A.type_ "submit"
-             ! A.name "register" $ do "完成注册"
+  getHtmlFromValidatableForm (stringValue $ mkFillInfoLink email vcode) mkSafeFillInfoForm
