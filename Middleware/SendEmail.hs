308059f4ac0c71c3e97e1bf4bbf6c8e5ca8da1e0
刘闽晟 <notcome@me.com>
2015-01-29 23:47:51 +0800

    revert to true sendemail

--- Middleware/SendEmail.hs
+++ Middleware/SendEmail.hs
@@ -6,20 +6,11 @@ import qualified Data.Text.Lazy as TL
 import Data.BBQ (Email(..))
 import Network.Mail.SMTP
 
-{-
-sendNotification :: String -> String -> String -> IO ()
-sendNotification recipent title content = do
+sendNotification :: Email -> String -> String -> IO ()
+sendNotification (Email recipent) title content = do
   let recipent' = T.pack recipent
   let title' = T.pack title
   let content' = TL.pack content
   sendMailWithLogin' "localhost" 587 "testuser" "testpassword" $ simpleMail
     (Address (Just "Yan.ac 消息通知") "notifications@yan.ac")
     [Address Nothing recipent'] [] [] title' [plainTextPart content']
--}
-
-sendNotification :: Email -> String -> String -> IO ()
-sendNotification (Email recipent) title content = do
-  putStrLn $ "Recipent: " ++ recipent
-  putStrLn $ "Title:    " ++ title
-  putStrLn $ "Content:"
-  putStrLn content
