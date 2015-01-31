2560a341f1f543700d98361b3d5b64ca5c8b46e6
刘闽晟 <notcome@me.com>
2015-01-31 18:49:59 +0800

    use CPP extension

--- Middleware/SendEmail.hs
+++ Middleware/SendEmail.hs
@@ -1,11 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, CPP #-}
 module Middleware.SendEmail where
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import qualified Data.Text.Lazy as TL
+import Data.String.UTF8
 import Data.BBQ (Email(..))
 import Network.Mail.SMTP
 
+#ifdef DEBUG
+sendNotification :: Email -> String -> String -> IO ()
+sendNotification (Email recipent) title content = do
+  print recipent
+  print title
+  T.putStrLn $ T.pack content
+
+#else
 sendNotification :: Email -> String -> String -> IO ()
 sendNotification (Email recipent) title content = do
   let recipent' = T.pack recipent
@@ -14,3 +24,4 @@ sendNotification (Email recipent) title content = do
   sendMailWithLogin' "localhost" 587 "testuser" "testpassword" $ simpleMail
     (Address (Just "Yan.ac 消息通知") "notifications@yan.ac")
     [Address Nothing recipent'] [] [] title' [plainTextPart content']
+#endif
