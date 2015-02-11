e5c4f65dcf1e2d8594a9e6232698480985eefda0
刘闽晟 <notcome@me.com>
2015-02-12 04:27:29 +0800

    grunt-traceur is so hard to use
    so I removed them.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,6 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    traceur: {
-      custom: {
-        files: [{
-          expand: true,
-          cwd: 'views/js-es6',
-          src: ['*.js'],
-          dest: 'views/js'
-        }]
-      }
-    },
     sass: {
       dist: {
         files: [{
@@ -25,9 +15,6 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-traceur');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-newer');
-
-  grunt.registerTask('default', ['newer:traceur'], ['sass']);
+  grunt.registerTask('default', ['sass']);
 };
