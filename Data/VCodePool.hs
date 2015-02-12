610111ef656f57457bbb51960ec9c395c8b8f23a
刘闽晟 <notcome@me.com>
2015-02-12 17:42:40 +0800

    VCodePool rewritten

--- Data/VCodePool.hs
+++ Data/VCodePool.hs
@@ -1,119 +1,113 @@
-{-# LANGUAGE DeriveDataTypeable, TemplateHaskell, FlexibleInstances,
-  TypeFamilies, FlexibleContexts, NamedFieldPuns #-}
+{-# LANGUAGE DeriveDataTypeable, TemplateHaskell, TypeFamilies, FlexibleInstances, FlexibleContexts, UndecidableInstances #-}
 
 module Data.VCodePool where
 
-import Data.Data       ( Data, Typeable )
-import Data.SafeCopy   ( base, deriveSafeCopy )
-import Data.IxSet      ( Indexable(..), IxSet(..), ixFun, ixSet )
+import Data.Data               (Data, Typeable)
+import Data.SafeCopy           (base, deriveSafeCopy, SafeCopy(..))
 
-import Control.Monad.Reader ( ask )
-import Control.Monad.State  ( get, put )
-import Control.Monad.Trans
-import Data.Data            ( Data, Typeable )
-import Data.Maybe
-import Data.Acid            ( Query, Update, makeAcidic )
-import Data.IxSet           ( (@=), Proxy(..), getOne, empty )
-import qualified Data.IxSet as IxSet
+import Control.Applicative     ((<$>), (<*>))
+import Control.Monad.Reader    (ask)
+import Control.Monad.State     (get, put)
+import Data.Acid
+import Data.Acid.Advanced
+
+import Data.IxSet
+    ( Indexable, IxSet(..), ixFun, ixSet
+    , empty, (@=), getOne
+    )
+import qualified Data.IxSet as Ix
+
+import System.Random
+import Data.Time               (formatTime)
+import Data.Time.Clock         (getCurrentTime)
+import System.Locale           (defaultTimeLocale)
+
 import Data.Account
 
-newtype ExpireTime = ExpireTime { unExpireTime :: Int }
+newtype ExpireTime = ExpireTime Int
   deriving (Eq, Ord, Data, Typeable, Show)
-newtype VCode = VCode { unVCode :: String }
+newtype VCode = VCode String
   deriving (Eq, Ord, Data, Typeable, Show)
-$(deriveSafeCopy 0 'base ''ExpireTime)  
+$(deriveSafeCopy 0 'base ''ExpireTime)
 $(deriveSafeCopy 0 'base ''VCode)
 
-data VCodeRecord k = VCodeRecord {
-    primaryKey :: k
-  , vcode      :: VCode
-  , expireTime :: ExpireTime
+data Record k = Record {
+    getKey    :: k
+  , getVCode  :: VCode
+  , getETime  :: ExpireTime
   } deriving (Eq, Ord, Data, Typeable, Show)
+$(deriveSafeCopy 0 'base ''Record)
 
-$(deriveSafeCopy 0 'base ''VCodeRecord)
+instance (Typeable k, Ord k) => Indexable (Record k) where
+  empty = ixSet
+    [ ixFun $ \bp -> [getKey bp]
+    , ixFun $ \bp -> [getVCode bp]
+    ]
 
-instance Indexable (VCodeRecord Email) where
-  empty = ixSet [ ixFun $ \bp -> [ primaryKey bp ] ]
-instance Indexable (VCodeRecord AccountId) where
-  empty = ixSet [ ixFun $ \bp -> [ primaryKey bp ] ]
+type RecordPool k = IxSet (Record k)
 
-type VCodePool k = IxSet (VCodeRecord k)
+newRecord :: (Typeable k, Ord k)
+  => k -> VCode -> ExpireTime -> Update (RecordPool k) ()
+newRecord key vcode etime = do
+  let record = Record key vcode etime
+  pool' <- Ix.updateIx key record <$> get
+  put pool'
 
-data VCodePools = VCodePools {
-    newAccountPool  :: VCodePool Email
-  , resetPasswdPool :: VCodePool Email
-  , cookiePool      :: VCodePool AccountId
-  } deriving (Data, Typeable, Show)
-$(deriveSafeCopy 0 'base ''VCodePools)
+validateRecord :: (Typeable k, Ord k)
+  => VCode -> ExpireTime -> Query (RecordPool k) (Maybe k)
+validateRecord vcode now = do
+  pool <- ask
+  case getOne $ pool @= vcode of
+    Nothing  -> return Nothing
+    Just Record { getKey = key, getETime = etime } ->
+      if etime < now
+      then return Nothing
+      else return $ Just key
 
-initialVCodePools :: VCodePools
-initialVCodePools = VCodePools empty empty empty
+removeRecord :: (Typeable k, Ord k)
+  => VCode -> Update (RecordPool k) ()
+removeRecord vcode = do
+  pool' <- Ix.deleteIx vcode <$> get
+  put pool'
 
-updateNewAccountPool  pools pool = pools { newAccountPool  = pool }
-updateResetPasswdPool pools pool = pools { resetPasswdPool = pool }
-updateCookiePool      pools pool = pools { cookiePool      = pool }
+-- Helper Functions --
+getNextVCode :: IO VCode
+getNextVCode = VCode . show <$> getStdRandom (randomR (100000000000000 :: Integer, 999999999999999 :: Integer))
 
-newVCodeRecord
-  :: (IxSet.Indexable (VCodeRecord k), Typeable k, Ord k)
-  => (VCodePools -> VCodePool k)
-  -> (VCodePools -> VCodePool k -> VCodePools)
-  -> k -> VCode -> ExpireTime -> Update VCodePools ()
-newVCodeRecord extract update key vcode expireTime = do
-  let record = VCodeRecord key vcode expireTime
-  pools <- get
-  let pool = extract pools
-  let pool' = IxSet.updateIx key record pool
-  put $ update pools pool'
+expireIn :: ExpireTime -> IO ExpireTime
+expireIn (ExpireTime ttl) = do
+  now <- (read <$> formatTime defaultTimeLocale "%s" <$> getCurrentTime) :: IO Int
+  return $ ExpireTime $ now + ttl
 
-insertNewAccountRecord  = newVCodeRecord newAccountPool updateNewAccountPool
-insertResetPasswdRecord = newVCodeRecord resetPasswdPool updateResetPasswdPool
-insertCookieRecord      = newVCodeRecord cookiePool updateCookiePool
+-- Template Haskell of Acid State breaks here.   --
+-- Writing those types manually here.            --
+-- Should use makeAcidic when this bug is fixed. --
+data NewRecord k = NewRecord k VCode ExpireTime
+  deriving (Typeable)
+$(deriveSafeCopy 0 'base ''NewRecord)
+instance (Typeable k, SafeCopy k) => Method (NewRecord k) where
+  type MethodResult (NewRecord k) = ()
+  type MethodState  (NewRecord k) = RecordPool k
+instance (Typeable k, SafeCopy k) => UpdateEvent (NewRecord k)
 
-verifyVCodeRecord
-  :: (IxSet.Indexable (VCodeRecord k), Typeable k, Ord k)
-  => (VCodePools -> VCodePool k)
-  -> k -> VCode -> ExpireTime -> Query VCodePools (Either String ())
-verifyVCodeRecord extract key givenCode now = do
-  pools <- ask
-  let pool = extract pools
-  case getOne $ pool @= key of
-    Nothing -> return $ (Left "验证记录不存在")
-    Just VCodeRecord { vcode, expireTime } ->
-      if vcode /= givenCode
-        then return $ (Left "无效的验证信息")
-        else if expireTime < now
-          then return $ (Left "验证信息已过期")
-          else return $ (Right ())
+data ValidateRecord k = ValidateRecord VCode ExpireTime
+  deriving (Typeable)
+$(deriveSafeCopy 0 'base ''ValidateRecord)
+instance (Typeable k, SafeCopy k) => Method (ValidateRecord k) where
+  type MethodResult (ValidateRecord k) = Maybe k
+  type MethodState  (ValidateRecord k) = RecordPool k
+instance (Typeable k, SafeCopy k) => QueryEvent (ValidateRecord k)
 
-verifyNewAccountRecord  = verifyVCodeRecord newAccountPool
-verifyResetPasswdRecord = verifyVCodeRecord resetPasswdPool
-verifyCookieRecord      = verifyVCodeRecord cookiePool
+data RemoveRecord k = RemoveRecord VCode
+  deriving (Typeable)
+$(deriveSafeCopy 0 'base ''RemoveRecord)
+instance (Typeable k, SafeCopy k) => Method (RemoveRecord k) where
+  type MethodResult (RemoveRecord k) = ()
+  type MethodState  (RemoveRecord k) = RecordPool k
+instance (Typeable k, SafeCopy k) => UpdateEvent (RemoveRecord k)
 
-deleteVCodeRecord
-  :: (IxSet.Indexable (VCodeRecord k), Typeable k, Ord k)
-  => (VCodePools -> VCodePool k)
-  -> (VCodePools -> VCodePool k -> VCodePools)
-  -> k -> Update VCodePools ()
-deleteVCodeRecord extract update key = do
-  pools <- get
-  let pool = extract pools
-  let pool' = IxSet.deleteIx key pool
-  put $ update pools pool'
-
-deleteNewAccountRecord  = deleteVCodeRecord newAccountPool updateNewAccountPool
-deleteResetPasswdRecord = deleteVCodeRecord resetPasswdPool updateResetPasswdPool
-
-getPools :: Query VCodePools VCodePools
-getPools = ask
-
-$(makeAcidic ''VCodePools
-  [ 'insertNewAccountRecord
-  , 'insertResetPasswdRecord
-  , 'insertCookieRecord
-  , 'verifyNewAccountRecord
-  , 'verifyResetPasswdRecord
-  , 'verifyCookieRecord
-  , 'deleteNewAccountRecord
-  , 'deleteResetPasswdRecord
-  , 'getPools
-  ])
+instance (Typeable k, SafeCopy k, Ord k) => IsAcidic (RecordPool k) where
+  acidEvents = [ UpdateEvent (\(NewRecord key vcode etime) -> newRecord key vcode etime)
+               , QueryEvent  (\(ValidateRecord vcode now)  -> validateRecord vcode now)
+               , UpdateEvent (\(RemoveRecord vcode)        -> removeRecord vcode)
+               ]
